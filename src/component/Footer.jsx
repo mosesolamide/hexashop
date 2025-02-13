4de7cbca293e3a8ca8ddb38d4b8aa39ca86735be
mosesolamide <olamidemoses132@gmail.com>
2025-02-13 08:27:57 +0100

    updated footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Footer(){
+    return(
+        <footer className='bg-[#1E1E1E] w-screen mt-10 text-white '>
+            <div className='grid grid-cols-[repeat(auto-fit,150px)] gap-6 justify-around py-5'>
+                <div className='flex flex-col gap-2'>
+                    <div className="flex items-center">
+                        <img src="/images/logo_white.png" alt="logo" className="w-[20px]" />
+                        <div className="flex flex-col ml-1 p-0">
+                            <h1 className="font-bold text-[8px] sm:text-[10px] md:text-sm">HEXASHOP</h1>
+                            <small className="text-[3px] sm:text-[5px] md:text-[8px]">ONLINE SHOPPING</small>
+                        </div>
+                    </div>
+                    <div className='text-[9px] flex flex-col gap-1'>
+                        <p><b>Address: </b>Lahore Road, Sheikhupura</p>
+                        <p><b>E-mail: </b>Example@gmail.com</p>
+                        <p><b>Phone: </b>08167893167</p>
+                    </div>
+                </div>
+                <div className='text-[9px] flex flex-col gap-1'>
+                    <p className='font-bold text-[10px]'>Shopping and Categories</p>
+                    <Link>Men's</Link>
+                    <Link>Women's Shopping</Link>
+                    <Link>Kid's Shopping</Link>
+                </div>
+                <div className='text-[9px] flex flex-col gap-1'>
+                    <p className='font-bold text-[10px]'>Useful Links</p>
+                    <Link>Home</Link>
+                    <Link>About Us</Link>
+                    <Link>Help</Link>
+                    <Link>Contact Us</Link>
+                </div>
+                <div className='text-[9px] flex flex-col gap-1'>
+                    <p className='font-bold text-[10px]'>Help Information</p>
+                    <Link>FAQ'S</Link>
+                    <Link>Shipping</Link>
+                    <Link>Tracking Id</Link>
+                </div>
+            </div>
+            <div className='bg-black text-[5px] py-3 text-center'>
+                Copyright @ 2022
+            </div>
+        </footer>
+    )
+}
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -5,15 +5,15 @@ import { MdOutlineShoppingCart } from "react-icons/md"
 export default function Header(){
     return(
         <header>
-            <nav className="flex items-center justify-around py-2 w-screen bg-bgColor">
+            <nav className="flex items-center justify-around py-2 sm:py-3 w-screen bg-bgColor">
                 <div className="flex items-center">
                     <img src="/images/logo.png" alt="logo" className="w-[20px]" />
                     <div className="flex flex-col ml-1 p-0">
-                        <h1 className="font-bold text-[8px]">HEXASHOP</h1>
-                        <small className="text-[3px]">ONLINE SHOPPING</small>
+                        <h1 className="font-bold text-[8px] sm:text-[10px] md:text-sm">HEXASHOP</h1>
+                        <small className="text-[3px] sm:text-[5px] md:text-[8px]">ONLINE SHOPPING</small>
                     </div>
                 </div>
-                <ul className="flex gap-3 text-[6px]">
+                <ul className="flex gap-3 text-[6px] sm:text-[10pxpx] md:text-sm">
                     <li><NavLink>Home</NavLink></li>
                     <li><NavLink>Men's</NavLink></li>
                     <li><NavLink>Women's</NavLink></li>
@@ -21,7 +21,7 @@ export default function Header(){
                     <li><NavLink>About Us</NavLink></li>
                     <li><NavLink>Contact Us</NavLink></li>
                 </ul>
-                <Link className="flex items-center md:gap-1 text-[6px]"><MdOutlineShoppingCart size={11} />Cart</Link>
+                <Link className="flex items-center md:gap-1 text-[6px] sm:text-[8px] md:text-sm w-[50px]"><MdOutlineShoppingCart />Cart</Link>
             </nav>
         </header>
     )
