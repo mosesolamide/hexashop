81f8702fe3c6baeaaab3cae0233192dfc114bbce
mosesolamide <olamidemoses132@gmail.com>
2025-03-08 19:17:30 +0100

    nw

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -5,7 +5,7 @@ export default function AboutUs(){
         <section className='bg-[url("images/about.png")] bg-cover bg-center w-full h-[940px] sm:h-[670px] object-cover relative flex justify-center py-2'>
             <div className='bg-black h-full w-full absolute opacity-[50%] inset-0'></div>
             <div className='absolute p-3'>
-                <b className='text-white flex justify-center'>About Us</b>
+                <b className='text-white flex justify-center md:text-[25px]'>About Us</b>
                 <div className='grid gap-2 mt-3 sm:grid-cols-3 sm:grid-row-3 sm:px-8'>
                     <div className='bg-[#FFA92E] py-4 px-4 sm:px-9 sm:col-span-2 flex flex-col justify-center'>
                         <b className='my-3'>Mission Statement</b>
